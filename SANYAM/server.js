5a276c73ad79b167213a053ffc43f7457fde618d
unknown <sayam.ajmera@gmail.com>
2023-01-20 14:58:19 +0530

    added

---
+++ SANYAM/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile('public/index.html',{root:__dirname})
+})
+
+app.get('/tree', (req, res) => {
+    res.sendFile('public/tree.html',{root:__dirname})
+})
+app.listen(3000, () => {
+    console.log("Hi Danny")
+})
